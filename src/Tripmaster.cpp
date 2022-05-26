c29c8e28716057fc436fa28c8c83e8e17af2fbe5
DawidCh <k4lo5h@poczta.fm>
2022-05-26 23:49:24 +0200

    Adding compensation.

--- src/Tripmaster.cpp
+++ src/Tripmaster.cpp
@@ -9,8 +9,10 @@
 #include <HT1621.h>
 #include <Wire.h>
 #include <HMC5883L.h>
+#include <MPU6050.h>
 
 HMC5883L compass;
+MPU6050 mpu;
 HT1621 compassLCD;
 HT1621 tripLCD;
 
@@ -40,37 +42,26 @@ void wheelPulse();
 float radToDeg(float);
 float applyDeclinationAngle(float);
 int modulo(int, int);
+void setupButtons();
+void setupCompass();
+void setupSerial();
+void setupLCDs();
+void printHelloMessage(char*, char*);
+void setupMPU();
+float tiltCompensate(Vector, Vector);
 
 //app
 void setup() {
-  pinMode(LED, OUTPUT);
-  pinMode(TRIP_UP, INPUT_PULLUP);
-  pinMode(TRIP_DOWN, INPUT_PULLUP);
-  pinMode(TRIP_RESET, INPUT_PULLUP);
-  pinMode(WHEEL_SENSOR, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(WHEEL_SENSOR), wheelPulse, FALLING);
-  
-  compassLCD.begin(12, 11, 10); // (cs, wr, Data, backlight)
-  compassLCD.clear();
-  tripLCD.begin(9, 8, 7); // (cs, wr, Data, backlight)
-  tripLCD.clear();
-  
-  Serial.begin(9600);
-  Wire.begin();
-
-  
-  while (!compass.begin())
-  {
-    compassLCD.print((char*) "-00001-");
-    delay(500);
-  }
-  compass.setSamples(HMC5883L_SAMPLES_8);
-  compass.setOffset(-32, 59);
-
-  tripLCD.print((char*) "HELL0");
-  compassLCD.print((char*) "M0T0");
   digitalWrite(LED, LOW);
-  delay(1500);
+  setupButtons();
+  setupLCDs();
+
+  setupSerial();
+  setupCompass();
+  setupMPU();
+
+  printHelloMessage("HELL0", "M0T0");
+  delay(2000);
 }
 
 void loop() {  
@@ -80,8 +71,15 @@ void loop() {
 }
 
 void updateCompass() {
-  Vector norm = compass.readNormalize();
-  float heading = radToDeg(atan2(norm.YAxis, norm.XAxis));
+  Vector mag = compass.readNormalize();
+  Vector acc = mpu.readScaledAccel();
+
+  float headingRad = tiltCompensate(mag, acc);
+  if (headingRad == -1000) {
+    headingRad = atan2(mag.YAxis, mag.XAxis);
+  }
+  
+  float heading = radToDeg(headingRad);
   heading = applyDeclinationAngle(heading);
   compassLCD.print(heading, (char*) "%5u*", 0);
 }
@@ -143,4 +141,78 @@ float applyDeclinationAngle(float degHeading) {
 
 int modulo(int x, int N){
     return (x % N + N) %N;
+}
+
+void setupButtons() {
+  pinMode(LED, OUTPUT);
+  pinMode(TRIP_UP, INPUT_PULLUP);
+  pinMode(TRIP_DOWN, INPUT_PULLUP);
+  pinMode(TRIP_RESET, INPUT_PULLUP);
+  pinMode(WHEEL_SENSOR, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(WHEEL_SENSOR), wheelPulse, FALLING);
+}
+
+void setupCompass() {
+  while (!compass.begin())
+  {
+    compassLCD.print((char*) "-00001-");
+    delay(500);
+  }
+  compass.setSamples(HMC5883L_SAMPLES_8);
+  compass.setOffset(-32, 59);
+}
+
+void setupMPU() {
+  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  {
+    delay(500);
+  }
+  mpu.setI2CMasterModeEnabled(false);
+  mpu.setI2CBypassEnabled(true) ;
+  mpu.setSleepEnabled(false);
+}
+
+void setupSerial() {
+  Serial.begin(9600);
+  Wire.begin();
+}
+
+void setupLCDs() {
+  compassLCD.begin(12, 11, 10); // (cs, wr, Data, backlight)
+  compassLCD.clear();
+  tripLCD.begin(9, 8, 7); // (cs, wr, Data, backlight)
+  tripLCD.clear();
+}
+
+void printHelloMessage(char* tripMsg, char* compassMsg) {
+  tripLCD.print(tripMsg);
+  compassLCD.print(compassMsg);
+}
+
+float tiltCompensate(Vector mag, Vector normAccel)
+{
+  // Pitch & Roll 
+  
+  float roll;
+  float pitch;
+  
+  roll = asin(normAccel.YAxis);
+  pitch = asin(-normAccel.XAxis);
+
+  if (roll > 0.78 || roll < -0.78 || pitch > 0.78 || pitch < -0.78)
+  {
+    return -1000;
+  }
+  
+    // Some of these are used twice, so rather than computing them twice in the algorithem we precompute them before hand.
+  float cosRoll = cos(roll);
+  float sinRoll = sin(roll);  
+  float cosPitch = cos(pitch);
+  float sinPitch = sin(pitch);
+  
+  // Tilt compensation
+  float Xh = mag.XAxis * cosPitch + mag.ZAxis * sinPitch;
+  float Yh = mag.XAxis * sinRoll * sinPitch + mag.YAxis * cosRoll - mag.ZAxis * sinRoll * cosPitch;
+ 
+  return atan2(Yh, Xh);
 }
